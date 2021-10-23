92eeb6041e718bc4d5837f66700720cdb82b3238
Anirudh <anirudhv@purdue.edu>
2021-10-23 15:53:50 -0400

    Updated website

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const hamburgerClick = () => {
+	document.getElementById("navbar-mobile").style.width = "100%";
+}
+
+const closeBar = () => {
+	document.getElementById("navbar-mobile").style.width = "0";
+}
+
+document.addEventListener("scroll", () => {
+	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerWidth || 0);
+	alert("vh");
+	alert(vh);
+	const homes = document.getElementsByClassName("home");
+	forEach(homes, (h) => {
+		h.removeClass("active");
+	});
+	const abouts = document.getElementsByClassName("about");
+	forEach(abouts, (a) => {
+		a.removeClass("active");
+	});
+	const teams = document.getElementsByClassName("team");
+	forEach(teams, (t) => {
+		t.removeClass("active");
+	});
+	const connects = document.getElementsByClassName("connect");
+	forEach(connects, (c) => {
+		c.removeClass("active");
+	});
+	switch(vh) {
+		case(vh >= 0 && vh < 100):
+			const elems1 = document.getElementsByClassName("home");
+			forEach(elems1, (e) => {
+				e.addClass("active");
+			});
+			break;
+		case(vh >= 100 && vh < 200):
+			const elems2 = document.getElementsByClassName("about");
+			forEach(elems, (e) => {
+				e.addClass("active");
+			});
+			break;
+		case(vh >= 200 && vh < 300):
+			const elems3 = document.getElementsByClassName("team");
+			forEach(elems3, (e) => {
+				e.addClass("active");
+			});
+			break;
+		case(vh >= 300 && vh < 400):
+			const elems4 = document.getElementsByClassName("connect");
+			forEach(elems4, (e) => {
+				e.addClass("active");
+			});
+			break;
+		default: break;
+	}
+});
+
